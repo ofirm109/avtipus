b4e769f3549fdf5380c8391e8c66d124c8d4affb
ofir <ofirm109@walla.com>
2025-06-19 00:45:49 +0300

    0046

---
+++ story_content/user.js
@@ -0,0 +1,247 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6DKordRdxo3":
+        Script1();
+        break;
+      case "621eATHXa72":
+        Script2();
+        break;
+      case "6Jtl5Mjazkb":
+        Script3();
+        break;
+      case "6ZcCLeDDs5i":
+        Script4();
+        break;
+      case "5Zo7w5Q2UnS":
+        Script5();
+        break;
+      case "6SVuy4m9PXT":
+        Script6();
+        break;
+      case "62MjNR772S7":
+        Script7();
+        break;
+      case "6aw8dddHFLj":
+        Script8();
+        break;
+  }
+}
+
+function Script1()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script2()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script3()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script4()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script5()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script6()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script7()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script8()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
